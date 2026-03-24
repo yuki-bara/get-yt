1f86ee492dbab0b103c7f08bf5f9a70d16afd378
yuki-bara <yukibara30@gmail.com>
2026-03-25 06:38:31 +0700

    add: color fo version and Can enter multiple URLs.
    more: code part of v1.1

--- src/get-yt.go
+++ src/get-yt.go
@@ -14,51 +14,53 @@ import (
 var version = "version"
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) < 2 {
 		fmt.Println("Args >> error: No URL or No Action")
 		return
 	}
 
 	if os.Args[1] == "-V" {
-		fmt.Println("Version: ", version)
+		fmt.Printf("\033[33m Version:\033[36m %s \033[0m\n", version)
 		return
 	}
 
-	if len(os.Args[1]) < 11 {
-		fmt.Println("Id >> error: No id")
-		return
+	for i := 1; i < len(os.Args); i++ {
+		if len(os.Args[i]) < 11 {
+			fmt.Println("Id >> error: No id")
+			return
+		}
+		videoID := os.Args[i][len(os.Args[i])-11:]
+
+		client := youtube.Client{}
+
+		video, err := client.GetVideo(videoID)
+		if err != nil {
+			fmt.Println("pull >> error: ", err)
+			return
+		}
+
+		formats := video.Formats.WithAudioChannels()
+		stream, _, err := client.GetStream(video, &formats[0])
+		if err != nil {
+			fmt.Println("Stream >> error: ", err)
+			return
+		}
+		defer stream.Close()
+
+		file, err := os.Create(video.Title + ".mp4")
+		if err != nil {
+			fmt.Println("file >> error:", err)
+			return
+		}
+		defer file.Close()
+
+		fmt.Printf("download: %s...\n", video.Title)
+		_, err = io.Copy(file, stream)
+		if err != nil {
+			fmt.Println("download >> error: ", err)
+			return
+		}
+
+		fmt.Println("succeed!")
 	}
-	videoID := os.Args[1][len(os.Args[1])-11:]
-
-	client := youtube.Client{}
-
-	video, err := client.GetVideo(videoID)
-	if err != nil {
-		fmt.Println("pull >> error: ", err)
-		return
-	}
-
-	formats := video.Formats.WithAudioChannels()
-	stream, _, err := client.GetStream(video, &formats[0])
-	if err != nil {
-		fmt.Println("Stream >> error: ", err)
-		return
-	}
-	defer stream.Close()
-
-	file, err := os.Create(video.Title + ".mp4")
-	if err != nil {
-		fmt.Println("file >> error:", err)
-		return
-	}
-	defer file.Close()
-
-	fmt.Printf("download: %s...\n", video.Title)
-	_, err = io.Copy(file, stream)
-	if err != nil {
-		fmt.Println("download >> error: ", err)
-		return
-	}
-
-	fmt.Println("succeed!")
 }
