4deaa667a1ea8a475d45c05d34e25f927c28162e
yuki-bara <yukibara30@gmail.com>
2026-03-27 21:55:36 +0700

    fix: remodel

--- src/main.go
+++ src/main.go
@@ -5,18 +5,15 @@ package main
 
 import (
 	"fmt"
-	"io"
+	net "get-yt/src/module"
 	"os"
-	"sync"
-
-	"github.com/kkdai/youtube/v2"
 )
 
 var version = "version"
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("Args >> error: No [URL/id] or No Action")
+		fmt.Println("Args >> error: No Action")
 		return
 	}
 
@@ -24,51 +21,11 @@ func main() {
 		fmt.Printf("\033[33m Version:\033[36m %s \033[0m\n", version)
 		return
 	}
-
-	var wg sync.WaitGroup
-
-	for i := 1; i < len(os.Args); i++ {
-		wg.Add(1)
-		go func(id string) {
-			defer wg.Done()
-			if len(id) < 11 {
-				fmt.Println("Id >> error: No url or id")
-				return
-			}
-			videoID := id[len(id)-11:]
-
-			client := youtube.Client{}
-
-			video, err := client.GetVideo(videoID)
-			if err != nil {
-				fmt.Println("pull >> error: ", err)
-				return
-			}
-
-			formats := video.Formats.WithAudioChannels()
-			stream, _, err := client.GetStream(video, &formats[0])
-			if err != nil {
-				fmt.Println("Stream >> error: ", err)
-				return
-			}
-			defer stream.Close()
-
-			file, err := os.Create(video.Title + ".mp4")
-			if err != nil {
-				fmt.Println("file >> error:", err)
-				return
-			}
-			defer file.Close()
-
-			fmt.Printf("download: %s...\n", video.Title)
-			_, err = io.Copy(file, stream)
-			if err != nil {
-				fmt.Println("download >> error: ", err)
-				return
-			}
-
-			fmt.Println("succeed!")
-		}(os.Args[i])
+	if len(os.Args) > 2 {
+		if os.Args[1] == "-mp4" {
+			net.Download_mp4(os.Args[2:])
+		} else if os.Args[1] == "-m4a" {
+			net.Download_m4a(os.Args[2:])
+		}
 	}
-	wg.Wait()
 }
---
+++ src/module/YouTube.go
@@ -0,0 +1,113 @@
+package net
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/kkdai/youtube/v2"
+)
+
+func Download_mp4(ID []string) {
+	var wg sync.WaitGroup
+	for i := 0; i < len(ID); i++ {
+		wg.Add(1)
+		go func(id string) {
+			defer wg.Done()
+			if len(id) < 11 {
+				fmt.Println("Id >> error: No url or id")
+				return
+			}
+			videoID := id[len(id)-11:]
+
+			client := youtube.Client{}
+
+			video, err := client.GetVideo(videoID)
+			if err != nil {
+				fmt.Println("pull >> error: ", err)
+				return
+			}
+
+			formats := video.Formats.WithAudioChannels()
+			stream, _, err := client.GetStream(video, &formats[0])
+			if err != nil {
+				fmt.Println("Stream >> error: ", err)
+				return
+			}
+			defer stream.Close()
+
+			file, err := os.Create(video.Title + ".mp4")
+			if err != nil {
+				fmt.Println("file >> error:", err)
+				return
+			}
+			defer file.Close()
+
+			fmt.Printf("download: %s...\n", video.Title)
+			_, err = io.Copy(file, stream)
+			if err != nil {
+				fmt.Println("download >> error: ", err)
+				return
+			}
+
+			fmt.Println("succeed!")
+		}(ID[i])
+	}
+	wg.Wait()
+}
+
+func Download_m4a(ID []string) {
+	var wg sync.WaitGroup
+	for i := 0; i < len(ID); i++ {
+		wg.Add(1)
+		go func(id string) {
+			defer wg.Done()
+			if len(id) < 11 {
+				fmt.Println("Id >> error: No url or id")
+				return
+			}
+			videoID := id[len(id)-11:]
+
+			client := youtube.Client{}
+
+			video, err := client.GetVideo(videoID)
+			if err != nil {
+				fmt.Println("pull >> error: ", err)
+				return
+			}
+
+			formats := video.Formats.WithAudioChannels()
+			var targetFormat *youtube.Format
+			for _, f := range formats {
+				if f.FPS == 0 {
+					targetFormat = &f
+					break
+				}
+			}
+			stream, _, err := client.GetStream(video, targetFormat)
+			if err != nil {
+				fmt.Println("Stream >> error: ", err)
+				return
+			}
+			defer stream.Close()
+
+			file, err := os.Create(video.Title + ".m4a")
+			if err != nil {
+				fmt.Println("file >> error:", err)
+				return
+			}
+			defer file.Close()
+
+			fmt.Printf("download: %s...\n", video.Title)
+			_, err = io.Copy(file, stream)
+			if err != nil {
+				fmt.Println("download >> error: ", err)
+				return
+			}
+
+			fmt.Println("succeed!")
+		}(ID[i])
+	}
+	wg.Wait()
+}
